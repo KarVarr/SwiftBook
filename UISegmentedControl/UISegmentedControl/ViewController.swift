d93419ec463f6ba95ac82dedd46523f3b772b600
KarVarr <megatr9n@gmail.com>
2022-12-21 20:24:33 +0300

    added UISegmented

---
+++ UISegmentedControl/UISegmentedControl/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UISegmentedControl
+//
+//  Created by Karen Vardanian on 21.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
