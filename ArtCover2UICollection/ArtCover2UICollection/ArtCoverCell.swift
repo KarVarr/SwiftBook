38619e3f052dd5ce964ec3701d55da330555e820
KarVarr <megatr9n@gmail.com>
2022-12-28 17:54:11 +0300

    added images and changed the estimaete size for imageCell bc it's changed size automaticly

---
+++ ArtCover2UICollection/ArtCover2UICollection/ArtCoverCell.swift
@@ -0,0 +1,13 @@
+//
+//  ArtCoverCell.swift
+//  ArtCover2UICollection
+//
+//  Created by Karen Vardanian on 28.12.2022.
+//
+
+import UIKit
+
+class ArtCoverCell: UICollectionViewCell {
+    
+    @IBOutlet weak var coverImageView: UIImageView!
+}
--- ArtCover2UICollection/ArtCover2UICollection/PreviewPage.swift
+++ ArtCover2UICollection/ArtCover2UICollection/PreviewPage.swift
@@ -7,20 +7,25 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
+private let reuseIdentifier = "ArtCover"
 
 class PreviewPage: UICollectionViewController {
+    
+    let imageNameArray = ["Alberto Ruiz - 7 Elements (Original Mix)",
+                          "Dave Wincent - Red Eye (Original Mix)",
+                          "E-Spectro - End Station (Original Mix)",
+                          "Edna Ann - Phasma (Konstantin Yoodza Remix)",
+                          "Ilija Djokovic - Delusion (Original Mix)",
+                          "John Baptiste - Mycelium (Original Mix)",
+                          "Lane 8 - Fingerprint (Original Mix)",
+                          "Mac Vaughn - Pink Is My Favorite Color (Alex Stein Remix)",
+                          "Metodi Hristov, Gallya - Badmash (Original Mix)",
+                          "Veerus, Maxie Devine - Nightmare (Original Mix)"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        
     }
 
     /*
@@ -35,54 +40,19 @@ class PreviewPage: UICollectionViewController {
 
     // MARK: UICollectionViewDataSource
 
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-
+   
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        return imageNameArray.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ArtCoverCell
     
-        // Configure the cell
+        cell.coverImageView.image = UIImage(named: imageNameArray[indexPath.row])
     
         return cell
     }
 
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
+ 
 
 }
