0e53d51a555b610c8ac099616baac83eadf7c2a8
KarVarr <megatr9n@gmail.com>
2023-01-01 21:02:36 +0300

    added pageViewController

---
+++ UIPageView/UIPageView/ContentViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ContentViewController.swift
+//  UIPageView
+//
+//  Created by Karen Vardanian on 01.01.2023.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    
+    @IBOutlet weak var presentTextLabel: UILabel!
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
