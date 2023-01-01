3dd4aa10cfc1f69d75fe8bc7e4e10b35dc1eaaab
KarVarr <megatr9n@gmail.com>
2023-01-01 21:30:37 +0300

    added array of emoji and array of texts

--- UIPageView/UIPageView/PageViewController.swift
+++ UIPageView/UIPageView/PageViewController.swift
@@ -8,22 +8,40 @@
 import UIKit
 
 class PageViewController: UIPageViewController {
-
+    
+    let presentsScreenContent = [
+        "Первая страница презентации, рассказывающая о том, что наше приложение из себя представляет",
+        "Вторая страница презентации, рассказывает о какой-то удобной фишке приложения",
+        "Третья страница презентации тоже рассказывает о чем то очень интересном",
+        "Ну и наконец последняя страница презентации с напутствием в добрый путь =)"
+    ]
+    
+    let emojiArray = ["😀","😠","🤬","🫠",]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        
+        if let contentViewController = showViewControllerAtIndex(0) {
+            setViewControllers([contentViewController], direction: .forward, animated: true)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func showViewControllerAtIndex(_ index: Int) -> ContentViewController? {
+        
+        guard index >= 0 else {return nil}
+        guard index < presentsScreenContent.count else {return nil}
+        guard let contentViewController = storyboard?.instantiateViewController(withIdentifier: "ContentViewController") as? ContentViewController else {return nil}
+        
+        contentViewController.presentText = presentsScreenContent[index]
+        contentViewController.emoji = emojiArray[index]
+        contentViewController.currentPage = index
+        contentViewController.numberOfPages = presentsScreenContent.count
+        
+        return contentViewController
     }
-    */
-
+    
+    
+    
 }
