5cd140a1062e8cd97e36f00bea1647f2581690f6
KarVarr <megatr9n@gmail.com>
2023-06-27 19:22:04 +0300

    added the MVC and delete Main storyboard for programmatically app

---
+++ GCD/GCD/Controller/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by Karen Vardanian on 27.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        addView()
+        settings()
+        layout()
+    }
+    
+    func addView() {
+        
+    }
+    
+    func settings() {
+        title = "GCD Application Test"
+    }
+    
+    func layout() {
+        
+    }
+
+
+}
+
