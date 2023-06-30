bf248e667ac8a9a38f8be7443a4b093c23f83b85
KarVarr <megatr9n@gmail.com>
2023-06-30 21:03:11 +0300

    added DispatchGroup

--- MyPlaygroundGCD.playground/Contents.swift
+++ MyPlaygroundGCD.playground/Contents.swift
@@ -3,35 +3,79 @@ import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-class SafeArray<Element> {
-    private var array = [Element]()
-    private let queue = DispatchQueue(label: "DispatchBarrier", attributes: .concurrent)
-    
-    public func append(element: Element) {
-        queue.async(flags: .barrier) {
-            self.array.append(element)
+//class SafeArray<Element> {
+//    private var array = [Element]()
+//    private let queue = DispatchQueue(label: "DispatchBarrier", attributes: .concurrent)
+//
+//    public func append(element: Element) {
+//        queue.async(flags: .barrier) {
+//            self.array.append(element)
+//        }
+//    }
+//
+//    public var elements: [Element] {
+//        var result = [Element]()
+//        queue.sync {
+//            result = self.array
+//        }
+//
+//        return result
+//    }
+//}
+//
+//var safeArray = SafeArray<Int>()
+//DispatchQueue.concurrentPerform(iterations: 10) { (index) in
+//    safeArray.append(element: index)
+//}
+//print("safeArray: \(safeArray.elements)")
+//
+//var array = [Int]()
+//DispatchQueue.concurrentPerform(iterations: 10) { (index) in
+//    array.append(index)
+//}
+//print("array: \(array)")
+
+let queue = DispatchQueue(label: "ru.swiftbook", attributes: .concurrent)
+let group = DispatchGroup()
+
+queue.async(group: group) {
+    for i in 0...10 {
+        if i == 10 {
+            print(i)
         }
     }
-    
-    public var elements: [Element] {
-        var result = [Element]()
-        queue.sync {
-            result = self.array
+}
+
+queue.async(group: group) {
+    for i in 0...20 {
+        if i == 20 {
+            print(i)
         }
-        
-        return result
     }
 }
 
-var safeArray = SafeArray<Int>()
-DispatchQueue.concurrentPerform(iterations: 10) { (index) in
-    safeArray.append(element: index)
+group.notify(queue: .main) {
+    print("All it's done!")
 }
-print("safeArray: \(safeArray.elements)")
 
-var array = [Int]()
-DispatchQueue.concurrentPerform(iterations: 10) { (index) in
-    array.append(index)
+let secondGroup = DispatchGroup()
+secondGroup.enter()
+queue.async(group: group) {
+    for i in 0...30 {
+        if i == 30 {
+            print(i)
+            sleep(2)
+            secondGroup.leave()
+        }
+    }
 }
-print("array: \(array)")
 
+let result = secondGroup.wait(timeout: .now() + 1)
+print(result)
+
+secondGroup.notify(queue: .main) {
+    print("It's all over in the group: secondGroup")
+    
+}
+
+print("this print might be upper then last one1 ")
