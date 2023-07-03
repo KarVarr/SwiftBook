85e690dd65c4f940e47c7a6beb551945a79a448c
KarVarr <megatr9n@gmail.com>
2023-07-03 11:13:24 +0400

    added DispatchWorkItem

--- MyPlaygroundGCD.playground/Contents.swift
+++ MyPlaygroundGCD.playground/Contents.swift
@@ -35,47 +35,69 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 //}
 //print("array: \(array)")
 
-let queue = DispatchQueue(label: "ru.swiftbook", attributes: .concurrent)
-let group = DispatchGroup()
 
-queue.async(group: group) {
-    for i in 0...10 {
-        if i == 10 {
-            print(i)
-        }
-    }
+//MARK: - DispatchGroup
+//
+//let queue = DispatchQueue(label: "ru.swiftbook", attributes: .concurrent)
+//let group = DispatchGroup()
+//
+//queue.async(group: group) {
+//    for i in 0...10 {
+//        if i == 10 {
+//            print(i)
+//        }
+//    }
+//}
+//
+//queue.async(group: group) {
+//    for i in 0...20 {
+//        if i == 20 {
+//            print(i)
+//        }
+//    }
+//}
+//
+//group.notify(queue: .main) {
+//    print("All it's done!")
+//}
+//
+//let secondGroup = DispatchGroup()
+//secondGroup.enter()
+//queue.async(group: group) {
+//    for i in 0...30 {
+//        if i == 30 {
+//            print(i)
+//            sleep(2)
+//            secondGroup.leave()
+//        }
+//    }
+//}
+//
+//let result = secondGroup.wait(timeout: .now() + 1)
+//print(result)
+//
+//secondGroup.notify(queue: .main) {
+//    print("It's all over in the group: secondGroup")
+//
+//}
+//
+//print("this print might be upper then last one1 ")
+        
+//MARK: - DispatchGroupItem
+
+let workItem = DispatchWorkItem(qos: .utility, flags: .detached) {
+    print("Perform workitem")
 }
 
-queue.async(group: group) {
-    for i in 0...20 {
-        if i == 20 {
-            print(i)
-        }
-    }
+//workItem.perform()
+let queue = DispatchQueue(label: "ru.swiftbook", attributes: .concurrent ,target: DispatchQueue.global(qos: .userInitiated))
+
+queue.asyncAfter(deadline: .now() + 1, execute: workItem)
+
+workItem.notify(queue: .main) {
+    print("workitem is complete ")
 }
 
-group.notify(queue: .main) {
-    print("All it's done!")
-}
-
-let secondGroup = DispatchGroup()
-secondGroup.enter()
-queue.async(group: group) {
-    for i in 0...30 {
-        if i == 30 {
-            print(i)
-            sleep(2)
-            secondGroup.leave()
-        }
-    }
-}
-
-let result = secondGroup.wait(timeout: .now() + 1)
-print(result)
-
-secondGroup.notify(queue: .main) {
-    print("It's all over in the group: secondGroup")
-    
-}
-
-print("this print might be upper then last one1 ")
+workItem.isCancelled
+workItem.cancel()
+workItem.isCancelled
