80d60154a5186fe5eeea783b06dd39bf8a2c2327
KarVarr <megatr9n@gmail.com>
2023-07-19 14:26:13 +0400

    added a new project for learning Firebase. ToDoFIRE

---
+++ ToDoFIRE/ToDoFIRE/Controller/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ToDoFIRE
+//
+//  Created by Karen Vardanian on 19.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        addViews()
+        settings()
+        layout()
+    }
+    
+    
+    func addViews() {
+        
+    }
+    
+    func settings() {
+        view.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+    }
+    
+    func layout() {
+        
+    }
+
+
+}
+
