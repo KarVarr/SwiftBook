5754c5b4661ae3599a985ecdc5bdf7db7385e184
KarVarr <megatr9n@gmail.com>
2023-07-21 14:03:38 +0400

    added Auth from firebase into func loginButtonTapped

--- ToDoFIRE/ToDoFIRE/Controller/ViewController.swift
+++ ToDoFIRE/ToDoFIRE/Controller/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     
@@ -53,94 +54,6 @@ class ViewController: UIViewController {
     }
     
     
-    //MARK: - VDL Section
-    
-    func addViews() {
-        view.addSubview(stackViewForLabels.customStack)
-        stackViewForLabels.customStack.addArrangedSubview(titleLabel.customLabel)
-        stackViewForLabels.customStack.addArrangedSubview(subTitleLabel.customLabel)
-        
-        view.addSubview(stackViewForTextField.customStack)
-        stackViewForTextField.customStack.addArrangedSubview(emailTextField.customTextFieldView)
-        stackViewForTextField.customStack.addArrangedSubview(passwordTextField.customTextFieldView)
-        
-        view.addSubview(stackViewForButtons.customStack)
-        stackViewForButtons.customStack.addArrangedSubview(loginButton.customButton)
-        stackViewForButtons.customStack.addArrangedSubview(registerButton.customButton)
-        
-    }
-    
-    func settings() {
-        view.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
-        
-        
-        stackViewSetting(forStackView: stackViewForLabels.customStack, withSpacing: 10)
-        stackViewSetting(forStackView: stackViewForTextField.customStack, withSpacing: 20)
-        stackViewSetting(forStackView: stackViewForButtons.customStack, withSpacing: 20)
-    }
-    
-    func settingsForLabels() {
-        titleLabel.customLabel.text = Helper.String.title
-        titleLabel.customLabel.font = Helper.Fonts.TamilSangamMN(withSize: 40)
-        
-        subTitleLabel.customLabel.text = Helper.String.subtitle
-        subTitleLabel.customLabel.font = Helper.Fonts.TamilSangamMN(withSize: 18)
-    }
-    
-    func settingsForTextFields() {
-        emailTextField.customTextFieldView.placeholder = Helper.String.email
-        emailTextField.customTextFieldView.layer.cornerRadius = 20
-        emailTextField.customTextFieldView.delegate = self
-        emailTextField.customTextFieldView.tag = 0
-        
-        passwordTextField.customTextFieldView.placeholder = Helper.String.password
-        passwordTextField.customTextFieldView.layer.cornerRadius = 20
-        passwordTextField.customTextFieldView.isSecureTextEntry = true
-        passwordTextField.customTextFieldView.delegate = self
-        passwordTextField.customTextFieldView.tag = 1
-        
-        addLeftView(to: emailTextField.customTextFieldView, width: 20)
-        addLeftView(to: passwordTextField.customTextFieldView, width: 20)
-    }
-    
-    func settingsForButtons() {
-        loginButton.customButton.backgroundColor = .white.withAlphaComponent(0.2)
-        loginButton.customButton.setTitle(Helper.String.login, for: .normal)
-        loginButton.customButton.titleLabel?.font = Helper.Fonts.TamilSangamMN(withSize: 24)
-        loginButton.customButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
-        loginButton.customButton.layer.cornerRadius = 20
-        
-        registerButton.customButton.titleLabel?.font = Helper.Fonts.TamilSangamMN(withSize: 16)
-        registerButton.customButton.setTitle(Helper.String.register, for: .normal)
-        
-    }
-    
-    func layout() {
-        let stackViewForLabels = stackViewForLabels.customStack
-        let stackViewForTextField = stackViewForTextField.customStack
-        let stackViewForButtons = stackViewForButtons.customStack
-        
-        NSLayoutConstraint.activate([
-            
-            stackViewForLabels.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
-            stackViewForLabels.heightAnchor.constraint(equalToConstant: 100),
-            stackViewForLabels.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-            stackViewForTextField.topAnchor.constraint(equalTo: stackViewForLabels.bottomAnchor, constant: 50),
-            stackViewForTextField.heightAnchor.constraint(equalToConstant: 100),
-            stackViewForTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            emailTextField.customTextFieldView.widthAnchor.constraint(greaterThanOrEqualToConstant: 250),
-            passwordTextField.customTextFieldView.widthAnchor.constraint(greaterThanOrEqualToConstant: 250),
-            
-            stackViewForButtons.topAnchor.constraint(equalTo: stackViewForTextField.bottomAnchor, constant: 50),
-            stackViewForButtons.heightAnchor.constraint(equalToConstant: 100),
-            stackViewForButtons.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            loginButton.customButton.widthAnchor.constraint(equalToConstant: 150),
-            registerButton.customButton.widthAnchor.constraint(equalToConstant: 150),
-            
-        ])
-        
-    }
     
     
     //MARK: - Functions
@@ -188,11 +101,41 @@ class ViewController: UIViewController {
         stackView.distribution = .fillEqually
     }
     
+    func displayWarningLabel(withText text: String) {
+        subTitleLabel.customLabel.text = text
+        
+        UIView.animate(withDuration: 3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseIn, animations: { [weak self] in
+            self?.subTitleLabel.customLabel.alpha = 1
+        }) { [weak self] complete in
+            self?.subTitleLabel.customLabel.alpha = 0
+        }
+        
+        subTitleLabel.customLabel.alpha = 0
+    }
+    
     @objc func loginButtonTapped() {
         let tasksVC = TasksViewController()
         
-        if let navigationController = self.navigationController {
-            navigationController.pushViewController(tasksVC, animated: true)
+        
+        guard let email = emailTextField.customTextFieldView.text, let password = passwordTextField.customTextFieldView.text, email != "", password != "" else {
+            displayWarningLabel(withText: "Info is incorrect")
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] user, error in
+            if error != nil {
+                self?.displayWarningLabel(withText: "Error occurred")
+                return
+            }
+            
+            if user != nil {
+                if let navigationController = self?.navigationController {
+                    navigationController.pushViewController(tasksVC, animated: true)
+                    return
+                }
+            }
+            
+            self?.displayWarningLabel(withText: "No such user")
         }
     }
     
--- ToDoFIRE/ToDoFIRE/Helpers/Extentions/ViewController + Ext.swift
+++ ToDoFIRE/ToDoFIRE/Helpers/Extentions/ViewController + Ext.swift
@@ -8,5 +8,95 @@
 import UIKit
 
 extension ViewController: UITextFieldDelegate {
+    //MARK: - VDL Section
+    
+    func addViews() {
+        view.addSubview(stackViewForLabels.customStack)
+        stackViewForLabels.customStack.addArrangedSubview(titleLabel.customLabel)
+        stackViewForLabels.customStack.addArrangedSubview(subTitleLabel.customLabel)
+        
+        view.addSubview(stackViewForTextField.customStack)
+        stackViewForTextField.customStack.addArrangedSubview(emailTextField.customTextFieldView)
+        stackViewForTextField.customStack.addArrangedSubview(passwordTextField.customTextFieldView)
+        
+        view.addSubview(stackViewForButtons.customStack)
+        stackViewForButtons.customStack.addArrangedSubview(loginButton.customButton)
+        stackViewForButtons.customStack.addArrangedSubview(registerButton.customButton)
+        
+    }
+    
+    func settings() {
+        view.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        
+        subTitleLabel.customLabel.alpha = 0
+        
+        
+        stackViewSetting(forStackView: stackViewForLabels.customStack, withSpacing: 10)
+        stackViewSetting(forStackView: stackViewForTextField.customStack, withSpacing: 20)
+        stackViewSetting(forStackView: stackViewForButtons.customStack, withSpacing: 20)
+    }
+    
+    func settingsForLabels() {
+        titleLabel.customLabel.text = Helper.String.title
+        titleLabel.customLabel.font = Helper.Fonts.TamilSangamMN(withSize: 40)
+        
+        subTitleLabel.customLabel.text = Helper.String.subtitle
+        subTitleLabel.customLabel.font = Helper.Fonts.TamilSangamMN(withSize: 18)
+    }
+    
+    func settingsForTextFields() {
+        emailTextField.customTextFieldView.placeholder = Helper.String.email
+        emailTextField.customTextFieldView.layer.cornerRadius = 20
+        emailTextField.customTextFieldView.delegate = self
+        emailTextField.customTextFieldView.tag = 0
+        
+        passwordTextField.customTextFieldView.placeholder = Helper.String.password
+        passwordTextField.customTextFieldView.layer.cornerRadius = 20
+        passwordTextField.customTextFieldView.isSecureTextEntry = true
+        passwordTextField.customTextFieldView.delegate = self
+        passwordTextField.customTextFieldView.tag = 1
+        
+        addLeftView(to: emailTextField.customTextFieldView, width: 20)
+        addLeftView(to: passwordTextField.customTextFieldView, width: 20)
+    }
+    
+    func settingsForButtons() {
+        loginButton.customButton.backgroundColor = .white.withAlphaComponent(0.2)
+        loginButton.customButton.setTitle(Helper.String.login, for: .normal)
+        loginButton.customButton.titleLabel?.font = Helper.Fonts.TamilSangamMN(withSize: 24)
+        loginButton.customButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        loginButton.customButton.layer.cornerRadius = 20
+        
+        registerButton.customButton.titleLabel?.font = Helper.Fonts.TamilSangamMN(withSize: 16)
+        registerButton.customButton.setTitle(Helper.String.register, for: .normal)
+        
+    }
+    
+    func layout() {
+        let stackViewForLabels = stackViewForLabels.customStack
+        let stackViewForTextField = stackViewForTextField.customStack
+        let stackViewForButtons = stackViewForButtons.customStack
+        
+        NSLayoutConstraint.activate([
+            
+            stackViewForLabels.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            stackViewForLabels.heightAnchor.constraint(equalToConstant: 100),
+            stackViewForLabels.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            stackViewForTextField.topAnchor.constraint(equalTo: stackViewForLabels.bottomAnchor, constant: 50),
+            stackViewForTextField.heightAnchor.constraint(equalToConstant: 100),
+            stackViewForTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            emailTextField.customTextFieldView.widthAnchor.constraint(greaterThanOrEqualToConstant: 250),
+            passwordTextField.customTextFieldView.widthAnchor.constraint(greaterThanOrEqualToConstant: 250),
+            
+            stackViewForButtons.topAnchor.constraint(equalTo: stackViewForTextField.bottomAnchor, constant: 50),
+            stackViewForButtons.heightAnchor.constraint(equalToConstant: 100),
+            stackViewForButtons.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginButton.customButton.widthAnchor.constraint(equalToConstant: 150),
+            registerButton.customButton.widthAnchor.constraint(equalToConstant: 150),
+            
+        ])
+        
+    }
     
 }
