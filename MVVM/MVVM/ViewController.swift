2f67dd7ede3b787c035d237fb43cabaafac53ca8
KarVarr <megatr9n@gmail.com>
2023-07-25 15:50:16 +0400

    added new project MVVM

---
+++ MVVM/MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Karen Vardanian on 25.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+
+
+}
+
