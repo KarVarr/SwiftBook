61179a04b4a053107d50b43e2a2f46af193742a2
KarVarr <megatr9n@gmail.com>
2023-07-30 16:34:51 +0400

    added model and value

--- Networking/Alamofire/Alamofire/Controller/ViewController.swift
+++ Networking/Alamofire/Alamofire/Controller/ViewController.swift
@@ -11,27 +11,53 @@ import Alamofire
 
 
 class ViewController: UIViewController {
+    var pri: Price?
     
     let customTable = TableViewView()
     let array = [Int]()
-    let url = "http://data.fixer.io/api/latest?access_key=a95f5542d5ed32ef80d6fec5f2694f09"
+    let url = "http://data.fixer.io/api/latest"
     let key = "a95f5542d5ed32ef80d6fec5f2694f09"
+    let base = "EUR"
+    let symbols = "USD, RUB"
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let param = ["access_key": key, "base": base, "symbols": symbols]
+        
         addView()
         settings()
         layout()
         
-        getPrice(url: url)
+        getPrice(url: url, parameters: param) { [weak self] result in
+            switch result {
+            case .success(let price):
+                self?.pri = Price(title: price.title, data: price.data)
+            case .failure(let error):
+                print("Error fetching data: \(error)")
+            }
+        }
+        
+        print(pri?.title)
     }
     
-    func getPrice(url: String) {
+    func getPrice(url: String, parameters: [String: String], completion: @escaping (Result<Price, Error>) -> Void) {
         
-        AF.request(url).responseJSON { response in
-            print(response)
+        AF.request(url, method: .get, parameters: parameters).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                print(value)
+                if let json = value as? [String: Any],
+                   let title = json["base"] as? String,
+                   let date = json["date"] as? String {
+                    let price = Price(title: title, data: date)
+                    completion(.success(price))
+                }
+            case .failure(let error):
+                print(error)
+            }
+            
         }
     }
     
---
+++ Networking/Alamofire/Alamofire/Model/Price.swift
@@ -0,0 +1,15 @@
+//
+//  Price.swift
+//  ALF
+//
+//  Created by Karen Vardanian on 30.07.2023.
+//
+
+import Foundation
+
+
+struct Price {
+    let title: String
+    let data: String
+    
+}
