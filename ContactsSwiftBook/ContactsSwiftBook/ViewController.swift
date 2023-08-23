9c935f5781bf6a285290774eee0e8040efbcaa5f
KarVarr <megatr9n@gmail.com>
2023-08-23 14:16:10 +0400

    added new project from the book for test

--- ContactsSwiftBook/ContactsSwiftBook/ViewController.swift
+++ ContactsSwiftBook/ContactsSwiftBook/ViewController.swift
@@ -12,8 +12,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+       
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = "string \(indexPath.row)"
+        configuration.secondaryText = "Pidr \(indexPath.row)"
+        configuration.textProperties.color = .red
+
+    
+        return cell
+    }
+    
+    
 }
 
