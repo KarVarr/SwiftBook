63c308ba94d4552afc5f37bc52af69bfd27e413f
KarVarr <megatr9n@gmail.com>
2023-08-24 19:13:22 +0400

    tested defferent style for navBar

--- NavigationApp/NavigationApp/ViewController.swift
+++ NavigationApp/NavigationApp/ViewController.swift
@@ -12,6 +12,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        title = "New one"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "item"
+        navigationItem.prompt = "test"
+        navigationController?.navigationBar.tintColor = .orange
+        navigationController?.navigationBar.barTintColor = .green.withAlphaComponent(0.7)
+        navigationController?.navigationBar.backgroundColor = .white.withAlphaComponent(0.8)
+        navigationItem.hidesSearchBarWhenScrolling = true
+        navigationItem.searchController?.isActive = true
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.red]
+        
     }
 
 
