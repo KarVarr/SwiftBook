42979ebdc98cd9dbc1ca1093c18aa8b94d92fe9a
KarVarr <megatr9n@gmail.com>
2023-08-26 00:03:07 +0400

    testing custom cell

---
+++ To-Do Manager/To-Do Manager/Model/Task.swift
@@ -0,0 +1,32 @@
+//
+//  Task.swift
+//  To-Do Manager
+//
+//  Created by Karen Vardanian on 25.08.2023.
+//
+
+import Foundation
+
+
+enum TaskPriority {
+    case normal
+    case important
+}
+
+enum TaskStatus {
+    case planned
+    case completed
+}
+
+
+protocol TaskProtocol {
+    var title: String { get set }
+    var type: TaskPriority { get set }
+    var status: TaskStatus { get set }
+}
+
+struct Task: TaskProtocol {
+    var title: String
+    var type: TaskPriority
+    var status: TaskStatus
+}
