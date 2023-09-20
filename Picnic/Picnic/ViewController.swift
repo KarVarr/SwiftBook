c0759e445248c4835c9d7d1920523b4acdca1f9c
KarVarr <megatr9n@gmail.com>
2023-09-20 17:28:45 +0400

    added images and maked animation for two baskets

--- Picnic/Picnic/ViewController.swift
+++ Picnic/Picnic/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var basketBottom: UIImageView!
+    @IBOutlet weak var basketTop: UIImageView!
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        view.layoutIfNeeded()
+        UIView.animate(withDuration: 0.7, delay: 1.0, options: .curveEaseOut, animations: {
+            var basketTopFrame = self.basketTop.frame
+            basketTopFrame.origin.y -= basketTopFrame.size.height
+            var basketBottomFrame = self.basketBottom.frame
+            basketBottomFrame.origin.y += basketBottomFrame.size.height
+            self.basketTop.frame = basketTopFrame
+            self.basketBottom.frame = basketBottomFrame
+        }, completion: { finished in
+                print("Basket doors opened!")
+                
+            })
+        self.view.layoutIfNeeded()
+        
     }
-
-
+    
+    
 }
 
