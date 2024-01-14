1c9b2c31a2fc473cb1c61454e45f13714d91bfc5
KarVarr <megatr9n@gmail.com>
2024-01-14 17:17:17 +0400

    added new Todo list testing project from Head Swift book

---
+++ Todo list/Todo list/Todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_listApp.swift
+//  Todo list
+//
+//  Created by Karen Vardanian on 14.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
