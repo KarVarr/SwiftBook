f88089753140f6f40db7450a9dd524b06565e5ae
KarVarr <megatr9n@gmail.com>
2024-01-15 21:47:27 +0400

    adden new project from book head first - Excecutive timer

---
+++ Executive Timer/Executive Timer/Executive_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Executive_TimerApp.swift
+//  Executive Timer
+//
+//  Created by Karen Vardanian on 15.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Executive_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
