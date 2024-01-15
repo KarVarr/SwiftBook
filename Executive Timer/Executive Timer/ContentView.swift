bdbbaf0b1b97a8aa9808027b429cdee31782f3fb
KarVarr <megatr9n@gmail.com>
2024-01-15 22:19:05 +0400

    adden two circles

--- Executive Timer/Executive Timer/ContentView.swift
+++ Executive Timer/Executive Timer/ContentView.swift
@@ -7,13 +7,41 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    let defaultTime: CGFloat = 20
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
+    @State private var timerRunning = false
+    @State private var countdownTime: CGFloat = 20 // Initial value, you may want to adjust this
+
+    var strokeStyle: StrokeStyle {
+        StrokeStyle(lineWidth: 15, lineCap: .round)
+    }
+
+    var buttonIcon: String {
+        timerRunning ? "pause.rectangle.fill" : "play.rectangle.fill"
+    }
+
+    var countdownColor: Color {
+        switch countdownTime {
+        case 6...: return Color.green
+        case 3...: return Color.yellow
+        default: return Color.red
+        }
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Circle()
+                .stroke(Color.gray.opacity(0.2), style: strokeStyle)
+                .frame(width: 300, height: 300)
+
+            Circle()
+                .trim(from: 0, to: 1 - ((defaultTime - countdownTime) / defaultTime))
+                .stroke(countdownColor, style: strokeStyle)
+                .rotationEffect(.degrees(-90))
+                .animation(.easeInOut) // You can use withAnimation here if needed
         }
         .padding()
     }
